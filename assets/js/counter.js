4dee2e97dcc069351f52b0f43e04a5c0a59069fd
Nataly <martineznatalyaysha@gmail.com>
2025-11-03 20:33:38 -0600

    nataly - modifiacion de ../assets/css/style.css,../assets/images/Auditor\303\255a.jpg,./assets/images/Auditor\303\255a_y_Cumplimiento.jpg,../assets/images/Constituci\303\263n.jpg,../assets/images/Constituci\303\263n_sociedad.jpg, ../assets/images/Cumplimiento_IMSS.jpg, ../assets/images/Finanzas.jpg,../assets/images/Finanzas_Sustentables.jpg,../assets/images/N\303\263mina.jpg,../assets/images/Vigilancia SAT.jpg, ../assets/images/asesoria.jpg, ../assets/images/ayuda.jpg,../assets/images/confianza_impulsa.jpg,../assets/images/contabilidad.jpg,../assets/images/contabilidad_integral.jpg,../assets/images/contableyestrat\303\251gica.jpg,../assets/images/cumplimiento.jpg, ../assets/images/estrategias.jpg, ../assets/images/mision.jpg,../assets/images/n\303\263mina_recursos.jpg,../assets/images/portafolio.jpg, ../assets/images/services_0.jpg,../assets/images/services_3.jpg, ../assets/images/testimonio_1.jpg, ../assets/images/testimonio_2.jpg,c ../assets/images/testimonio_2.jpg,cc ../assets/images/testimonio_3.jpg,../assets/images/testimonio_4.jpg,c../assets/images/testimonio_5.jpg, ../assets/images/testimonio_6.jpg,../assets/images/vision.jpg,../cases.html,c../index.html,../services.html,../two-column.html

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,13 +1,37 @@
-$(document).ready(function () {
+$(document).ready(function () { 
     $('.counter').each(function () {
+        // Guardamos el valor original (puede incluir signos, %, +, etc.)
+        var originalText = $(this).text();
+        
+        // Extraemos solo el número del texto
+        var number = parseFloat(originalText.replace(/[^0-9\.\-]/g, ''));
+
+        // Detectamos si el texto original tiene un signo "+" al principio o al final
+        var hasPlus = /\+/.test(originalText);
+
+        // Detectamos si tiene algún símbolo al final (como % o cualquier otro)
+        var suffix = originalText.replace(/[0-9\.\+\-]/g, '').trim();
+
+        // Animamos el número
         $(this).prop('Counter', 0).animate({
-            Counter: $(this).text()
+            Counter: number
         }, {
             duration: 4000,
             easing: 'swing',
-            
             step: function (now) {
-                $(this).text(Math.ceil(now));
+                var formatted = Math.ceil(now);
+
+                // Si tenía el signo "+", lo colocamos antes del número
+                if (hasPlus) {
+                    formatted = '+' + formatted;
+                }
+
+                // Si tenía algún sufijo (como %), lo agregamos al final
+                if (suffix) {
+                    formatted += suffix;
+                }
+
+                $(this).text(formatted);
             }
         });
     });
